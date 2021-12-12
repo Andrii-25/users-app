316367af9f625b9cf9d885d498515d5bd136adc9
Andrii-25 <kotsupyr.a@gmail.com>
2021-12-12 12:46:18 +0200

    delete comments

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
-// const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const jwt = require("./utils/jwt");
 const errorHandler = require("./utils/error-handler");
-require("dotenv/config");
 
 const app = express();
 
@@ -16,14 +14,6 @@ app.use(cors());
 app.use(jwt());
 app.use("/users", require("./controllers/user.controller"));
 app.use(errorHandler);
-
-// mongoose.connect(
-//   process.env.DB_CONNECTION,
-//   { useNewUrlParser: true, useUnifiedTopology: true },
-//   () => {
-//     console.log(`Connected to DB: ${process.env.DB_CONNECTION}`);
-//   }
-// );
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("App is running...");
